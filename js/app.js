6567748455ae88432c854f5a370ce1c2f3874911
hamstomper11 <hamstomper11@gmail.com>
2024-04-25 00:33:31 +0000

    ye

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new materialstyle.Tooltip(tooltipTriggerEl)
+});
+
 // Initialize Ripple
 const rippleSurface = Array.prototype.slice.call(document.querySelectorAll('.ripple-surface'))
 rippleSurface.map(s => {
   return new mdc.ripple.MDCRipple(s)
-})
-
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new materialstyle.Tooltip(tooltipTriggerEl)
-})
+});

42a49c95c68b384ee3ada5975df3584bbf82e334
hamstomper11 <hamstomper11@gmail.com>
2024-07-16 14:00:14 +0000

    ye

--- js/app.js
+++ js/app.js
@@ -8,3 +8,19 @@ const rippleSurface = Array.prototype.slice.call(document.querySelectorAll('.rip
 rippleSurface.map(s => {
   return new mdc.ripple.MDCRipple(s)
 });
+
+// Initialize Text fields
+var textFieldList = [].slice.call(document.querySelectorAll('.form-control'))
+var textFields = textFieldList.map(function (textField) {
+  return new materialstyle.TextField(textField)
+})
+
+var myModal = document.getElementById('myModal')
+myModal.addEventListener('shown.bs.modal', function (event) {
+  // Redraw Text Field
+  var textFields = this.querySelectorAll('.form-control');
+  for (const [, value] of Object.entries(textFields)) {
+    var textFieldInstance = materialstyle.TextField.getOrCreateInstance(value)
+    textFieldInstance.redraw();
+  }
+});
